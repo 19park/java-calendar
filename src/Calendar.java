5b421c45e9b2867e9c4c912a60513dd71b410d48
pk-jh <94junghwan@gmail.com>
2020-02-06 19:43:54 +0900

    add return Condition

--- src/Calendar.java
+++ src/Calendar.java
@@ -17,15 +17,26 @@ public class Calendar {
 
     public static void main(String[] ar) {
         Calendar cal = new Calendar();
+
         // 숫자를 입력받아 해당하는 달의 최대 일수를 출력
+        String PROMPT = "cal> ";
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("반복횟수를 입력하세요.");
-        int repeat = scanner.nextInt();
 
-        for (int i = 0; i < repeat; i++) {
+        int month = 1;
+        while (true) {
             System.out.println("달을 입력하세용 ㅋ");
-            int month = scanner.nextInt();
+            System.out.print(PROMPT);
+            month = scanner.nextInt();
 
+            if (month == -1) {
+                break;
+            }
+
+            if (month > 12) {
+                continue;
+            }
             System.out.printf("%d월은 %d일까지 있슴ㅋ.\n", month, cal.getMaxDaysOfMonth(month));
         }
 
